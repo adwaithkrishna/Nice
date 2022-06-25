9b96ac1944f3adef2eab93b4a8f3323c049eb1bc
adwaithkrishna <adwaithkrishna17092002@gmail.com>
2022-06-26 02:47:00 +0530

    Send jsk traceback to channels instead

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
 import config
+import jishaku
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='-', intents=intents)
+jishaku.Flags.NO_DM_TRACEBACK = True
 
 
 @bot.event
