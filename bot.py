3cd18c2e7f9c27574966d3e67d268df89e2f94b5
adwaithkrishna <adwaithkrishna17092002@gmail.com>
2022-07-19 04:57:21 +0530

    Subclass Bot to add setup_hook

--- bot.py
+++ bot.py
@@ -1,17 +1,27 @@
+import aiohttp
 import discord
 from discord.ext import commands
-import config
 import jishaku
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='-', intents=intents)
-jishaku.Flags.NO_DM_TRACEBACK = True
+import config
 
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
+class Nice(commands.Bot):
+    
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(command_prefix="-", intents=intents)
+
+    async def setup_hook(self) -> None:
+        self.session = aiohttp.ClientSession()
+        jishaku.Flags.NO_DM_TRACEBACK = True
+
+    async def on_ready(self):
+        print(f"Logged in as {bot.user}")
+
+
+bot = Nice()
 
 
 @bot.command()
@@ -36,4 +46,5 @@ async def reload(ctx, extension):
     await bot.reload_extension(extension)
     await ctx.send(f"Reloaded {extension}")
 
-bot.run(config.token)
+if __name__ == '__main__':
+    bot.run(config.token)
